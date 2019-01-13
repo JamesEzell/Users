54fceb53114e46c002136ede7efb1e0f43201031
James Ezell <jamespaulezell@gmail.com>
2019-01-12 20:31:51 -0800

    Complete 01122031

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Users.Models;
+using Users.Infrastructure;
 
 namespace Users
 {
@@ -15,13 +16,23 @@ namespace Users
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPasswordValidator<AppUser>,CustomPasswordValidator>();
+
+            services.AddTransient<IUserValidator<AppUser>,CustomUserValidator>();
+
             services.AddDbContext<AppIdentityDbContext>(options =>
             options.UseSqlServer(
             Configuration["Data:SportStoreIdentity:ConnectionString"]));
 
-            services.AddIdentity<AppUser, IdentityRole>()
-            .AddEntityFrameworkStores<AppIdentityDbContext>()
-            .AddDefaultTokenProviders();
+            services.AddIdentity<AppUser, IdentityRole>(opts => {
+                opts.User.RequireUniqueEmail = true;
+                //opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+                    opts.Password.RequiredLength = 6;
+                    opts.Password.RequireNonAlphanumeric = false;
+                    opts.Password.RequireLowercase = false;
+                    opts.Password.RequireUppercase = false;
+                    opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
 
             services.AddMvc();
         }
