00663186b402a664b76aaadb14eb9407f450f2f2
James Ezell <jamespaulezell@gmail.com>
2019-01-31 21:35:57 -0800

    finished chapter 30 013119

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -2,9 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Users.Models;
 using Users.Infrastructure;
+using System.Security.Claims;
 
 namespace Users
 {
@@ -19,6 +22,36 @@ namespace Users
             services.AddTransient<IPasswordValidator<AppUser>,CustomPasswordValidator>();
 
             services.AddTransient<IUserValidator<AppUser>,CustomUserValidator>();
+
+            services.AddSingleton<IClaimsTransformation, LocationClaimsProvider>();
+
+            services.AddTransient<IAuthorizationHandler, BlockUsersHandler>();
+
+            services.AddTransient<IAuthorizationHandler, DocumentAuthorizationHandler>();
+
+            services.AddAuthorization(opts => {
+                opts.AddPolicy("DCUsers", policy => {
+                    policy.RequireRole("Users");
+                    policy.RequireClaim(ClaimTypes.StateOrProvince, "DC");
+                });
+                opts.AddPolicy("NotBob", policy => {
+                    policy.RequireAuthenticatedUser();
+                    policy.AddRequirements(new BlockUsersRequirement("Bob"));
+                });
+                opts.AddPolicy("AuthorsAndEditors", policy =>
+                {
+                    policy.AddRequirements(new DocumentAuthorizationRequirement
+                    {
+                        AllowAuthors = true,
+                        AllowEditors = true
+                    });
+                });
+            });
+
+            services.AddAuthentication().AddGoogle(opts => {
+                opts.ClientId = "<enter client id here>";
+                opts.ClientSecret = "<enter client secret here>";
+            });
 
             services.AddDbContext<AppIdentityDbContext>(options =>
             options.UseSqlServer(
